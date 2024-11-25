07e2e10f96b8cc13328088b85eaa912f9fc860b1
Yosef Hayim <yosefisabag+03@gmail.com>
2024-11-25 20:12:12 +0200

    add fonts and almost finish home

--- client/social-mock-front/src/Homepage/Homepage.jsx
+++ client/social-mock-front/src/Homepage/Homepage.jsx
@@ -3,14 +3,14 @@ import HomeImage from "/public//Homepage-image.svg";
 
 const Homepage = () => {
   return (
-    <div className="max-w-sm rounded-[2.2em] border border-white">
+    <div className="mt-[1em] max-w-sm rounded-[2.2em]">
       <img
         src={HomeImage}
         alt="Baba-gram logo image"
         className="rounded-[2em] p-2"
       />
       <div className="flex w-full flex-col items-center justify-center">
-        <h1 className="mb-[1em] mt-[1.5em] text-center text-2xl font-semibold">
+        <h1 className="mb-[1em] mt-[1.5em] text-center text-2xl font-black">
           Discover your <br />
           Dream job Here
         </h1>
@@ -21,12 +21,12 @@ const Homepage = () => {
       </div>
       <div className="mb-[2em] flex w-full items-center justify-center gap-2">
         <Link to="/register" className="w-full">
-          <button className="w-full translate-x-[15px] rounded-[0.7em] border border-white p-3 hover:bg-white active:bg-white">
+          <button className="w-full translate-x-[15px] rounded-[0.7em] border border-white p-3">
             Register
           </button>
         </Link>
         <Link to="/login" className="w-full">
-          <button className="w-full translate-x-[-15px] rounded-[0.5em] border border-white p-3 hover:bg-white active:bg-white">
+          <button className="w-full translate-x-[-15px] rounded-[0.5em] border border-white p-3">
             Sign in
           </button>
         </Link>
