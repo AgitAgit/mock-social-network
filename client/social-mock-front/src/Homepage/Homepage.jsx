b405866a35812716ca4c41e955b9a353f872cdc6
YosefHayim <yosefisabag+03@gmail.com>
2024-11-25 16:13:01 +0200

    fsd

--- client/social-mock-front/src/Homepage/Homepage.jsx
+++ client/social-mock-front/src/Homepage/Homepage.jsx
@@ -1,7 +1,27 @@
+import { Link } from "react-router-dom";
+
 const Homepage = () => {
-  return <div>
-    <div className={styles.}></div>
-  </div>;
+  return (
+    <div className="bg-slate-500 max-w-sm">
+      <div>
+        <img src="" alt="" />
+      </div>
+      <h1>Discover your Dream job Here</h1>
+      <p>
+        Explore all the most exciting job roles based on your interests and
+        study major
+      </p>
+
+      <div className="flex gap-5">
+        <Link to="/register">
+          <button className="w-20 p-2 rounded bg-slate-100">Register</button>
+        </Link>
+        <Link to="/login">
+          <button className="w-20 p-2 rounded bg-slate-100 ">Login</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
