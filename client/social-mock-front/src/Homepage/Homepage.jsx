01ed042378a67975e8e8f9d679dd95c0622a48bf
Yosef Hayim <yosefisabag+03@gmail.com>
2024-11-25 20:26:30 +0200

    fds

--- client/social-mock-front/src/Homepage/Homepage.jsx
+++ client/social-mock-front/src/Homepage/Homepage.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import HomeImage from "/public//Homepage-image.svg";
+import HomeImage from "/public/images/Homepage-image.svg";
 
 const Homepage = () => {
   return (
@@ -11,8 +11,8 @@ const Homepage = () => {
       />
       <div className="flex w-full flex-col items-center justify-center">
         <h1 className="mb-[1em] mt-[1.5em] text-center text-2xl font-black">
-          Discover your <br />
-          Dream job Here
+          Welcome to Babagram <br />
+          Flip-flop vibes.
         </h1>
         <p className="mb-[3em] w-[17em] text-center">
           Explore all the most exciting job roles based on your interests And
@@ -21,12 +21,12 @@ const Homepage = () => {
       </div>
       <div className="mb-[2em] flex w-full items-center justify-center gap-2">
         <Link to="/register" className="w-full">
-          <button className="w-full translate-x-[15px] rounded-[0.7em] border border-white p-3">
+          <button className="w-full rounded-[0.7em] border border-white bg-white p-3 hover:bg-black hover:text-white">
             Register
           </button>
         </Link>
         <Link to="/login" className="w-full">
-          <button className="w-full translate-x-[-15px] rounded-[0.5em] border border-white p-3">
+          <button className="w-full rounded-[0.5em] border border-white p-3 hover:bg-black hover:text-white">
             Sign in
           </button>
         </Link>
