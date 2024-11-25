8c772b0e93a1747605526cbc0a7a3bdbc3a629fb
Yosef Hayim <yosefisabag+03@gmail.com>
2024-11-25 19:34:26 +0200

    כגד

--- client/social-mock-front/src/Homepage/Homepage.jsx
+++ client/social-mock-front/src/Homepage/Homepage.jsx
@@ -3,32 +3,32 @@ import HomeImage from "/public//Homepage-image.svg";
 
 const Homepage = () => {
   return (
-    <div className="max-w-sm rounded-[2.2em] h-[37em]">
+    <div className="max-w-sm rounded-[2.2em] border border-white">
       <img
         src={HomeImage}
         alt="Baba-gram logo image"
         className="rounded-[2em] p-2"
       />
-
-      <div className="flex flex-col justify-center items-center w-full">
-        <h1 className="font-semibold mb-[1em] text-center">
+      <div className="flex w-full flex-col items-center justify-center">
+        <h1 className="mb-[1em] mt-[1.5em] text-center text-2xl font-semibold">
           Discover your <br />
           Dream job Here
         </h1>
-        <p className="font-thin text-center w-[17em] mb-[2em]">
+        <p className="mb-[3em] w-[17em] text-center">
           Explore all the most exciting job roles based on your interests And
           study major
         </p>
       </div>
-
-      <div className="flex items-center justify-center gap-1 text-[0.7em]">
-        <Link to="/register">
-          <button className="w-[6em] p-2 rounded bg-slate-100 ">
+      <div className="mb-[2em] flex w-full items-center justify-center gap-2">
+        <Link to="/register" className="w-full">
+          <button className="w-full translate-x-[15px] rounded-[0.7em] border border-white p-3 hover:bg-white active:bg-white">
             Register
           </button>
         </Link>
-        <Link to="/login">
-          <button className="w-[6em] p-2 rounded bg-slate-100 ">Login</button>
+        <Link to="/login" className="w-full">
+          <button className="w-full translate-x-[-15px] rounded-[0.5em] border border-white p-3 hover:bg-white active:bg-white">
+            Sign in
+          </button>
         </Link>
       </div>
     </div>
