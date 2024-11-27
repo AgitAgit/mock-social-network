286eda75bc91074c85e8cc1d33e1f77b0a869144
HavivBitton <havivbiton4@gmail.com>
2024-11-27 16:03:56 +0200

    create register page

---
+++ client/social-mock-front/src/Components/sign in and register/Register.jsx
@@ -0,0 +1,29 @@
+//Import
+import { Link } from "react-router-dom";
+
+// Import Components
+import Input from "./Input.jsx";
+import SubmitBtn from "./SubmitBtn.jsx";
+
+const Register = () => {
+  return (
+    <div className=" h-screen flex-col content-center">
+      <div>
+        <div className="my-10 flex flex-col items-center w-full">
+          <h1 className="text-2xl">Welcome to Babagram!</h1>
+          <h3 className="w-1/2">Few more step and your in</h3>
+        </div>
+        <div className="flex flex-col gap-4">
+          <Input type={"text"} placeholder={"Full Name"} />
+          <Input type={"text"} placeholder={"Email"} />
+          <Input type={"text"} placeholder={"User Name"} />
+          <Input type={"password"} placeholder={"PassWord"} />
+
+          <SubmitBtn btnPlaceholder={"Register"} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
