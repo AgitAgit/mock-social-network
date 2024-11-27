7c759dee2e408ddeb87510d6eca4031bb91fa920
HavivBitton <havivbiton4@gmail.com>
2024-11-27 15:08:26 +0200

    merge haviv

--- client/social-mock-front/src/App.jsx
+++ client/social-mock-front/src/App.jsx
@@ -1,15 +1,11 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
 
 //Import Components
 import HomePage from "./Components/HomePage";
 import Register from "./Components/Register.jsx";
 import SignIn from "./Components/SignIn/SignIn.jsx";
 import Profile from "./Components/Profile.jsx";
-=======
-import Homepage from "./Homepage/Homepage";
->>>>>>> yosef
 
 function App() {
   return (
