e8880010d65053e080fd7208f4229a707dae3474
Amit Kubani <amitkubani@gmail.com>
2024-12-01 11:26:33 +0200

    commit

--- server/routes/postsRoute.js
+++ server/routes/postsRoute.js
@@ -1,8 +1,11 @@
 const express = require("express");
 
 const { addPost, getAllPosts, getPostById } = require("../controllers/postsController.js");
+const { authUser } = require("../middleware/authUser.js");
 
 const router = express.Router();
+
+router.use("/", authUser);
 
 router.get("/", getAllPosts);
 
