6b74568dd5fa1757897ee591c781bc541eb158fb
yosefhayim <yosefisabag+03@gmail.com>
2024-12-02 16:38:05 +0200

    finish settings account page

--- client/src/Components/AccountOptions/AccountOptions.jsx
+++ client/src/Components/AccountOptions/AccountOptions.jsx
@@ -44,7 +44,27 @@ import { CiAt } from "react-icons/ci";
 import { FaRegComment } from "react-icons/fa";
 import { TiArrowSync } from "react-icons/ti";
 import { AiOutlineFontSize } from "react-icons/ai";
+import { FaRegStar } from "react-icons/fa";
+import { BsBellSlash } from "react-icons/bs";
+import { TbPhotoVideo } from "react-icons/tb";
+import { IoHeartDislikeOutline } from "react-icons/io5";
+import { HiDownload } from "react-icons/hi";
+import { CiMobile3 } from "react-icons/ci";
+import { LiaUniversalAccessSolid } from "react-icons/lia";
+import { LiaLanguageSolid } from "react-icons/lia";
+import { BsFillFileEarmarkBarGraphFill } from "react-icons/bs";
+import { TbHomeShield } from "react-icons/tb";
+import { MdSupport } from "react-icons/md";
 
+import { MdOutlinePrivacyTip } from "react-icons/md";
+import { IoPersonOutline } from "react-icons/io5";
+import { MdInfoOutline } from "react-icons/md";
+import { FaWhatsapp } from "react-icons/fa6";
+import { FaThreads } from "react-icons/fa6";
+import { RiFacebookCircleLine } from "react-icons/ri";
+import { AiOutlineInbox } from "react-icons/ai";
+import { MdOutlineVerified } from "react-icons/md";
+import { VscGraph } from "react-icons/vsc";
 
 
 
@@ -129,16 +149,55 @@ const AccountOptions = () => {
 <OptionTab icon={<AiOutlineFontSize />} arrowIcon={<ArrowForwardIcon />} title="Hidden Words" />
 <OptionTab icon={<MdPersonAdd />} arrowIcon={<ArrowForwardIcon />} title="Follow and Invite Friends" />
 <hr />
-
 <h2 className="text-gray-500">What you see</h2>
-<OptionTab icon={<MdFavoriteBorder />} arrowIcon={<ArrowForwardIcon />} title="Favorites" />
-<OptionTab icon={<MdOutlineVolumeOff />} arrowIcon={<ArrowForwardIcon />} title="Muted Accounts" />
-<OptionTab icon={<MdOutlineContentPaste />} arrowIcon={<ArrowForwardIcon />} title="Suggested Content" />
-<OptionTab icon={<MdOutlineThumbUp />} arrowIcon={<ArrowForwardIcon />} title="Like and Share Counts" />
+<OptionTab icon={<FaRegStar />} arrowIcon={<ArrowForwardIcon />} title="Favorites" />
+<OptionTab icon={<BsBellSlash />} arrowIcon={<ArrowForwardIcon />} title="Muted Accounts" />
+<OptionTab icon={<TbPhotoVideo />} arrowIcon={<ArrowForwardIcon />} title="Suggested Content" />
+<OptionTab icon={<IoHeartDislikeOutline />} arrowIcon={<ArrowForwardIcon />} title="Like and Share Counts" />
 <hr />
 <h2 className="text-gray-500">Your app and media</h2>
-<OptionTab icon={<MdOutlineDevices />} arrowIcon={<ArrowForwardIcon />} title="Device Permissions" />
-<OptionTab icon={<MdDownload />} arrowIcon={<ArrowForwardIcon />} title="Archiving and Downloading" />
+<OptionTab icon={<CiMobile3 />} arrowIcon={<ArrowForwardIcon />} title="Device permissions" />
+<OptionTab icon={<HiDownload />} arrowIcon={<ArrowForwardIcon />} title="Archiving and downloading" />
+<OptionTab icon={<LiaUniversalAccessSolid />} arrowIcon={<ArrowForwardIcon />} title="Accessibility and translations" />
+<OptionTab icon={<LiaLanguageSolid />} arrowIcon={<ArrowForwardIcon />} title="Language" />
+<OptionTab icon={<BsFillFileEarmarkBarGraphFill />} arrowIcon={<ArrowForwardIcon />} title="Media quality" />
+<OptionTab icon={<MdOutlineDevices />} arrowIcon={<ArrowForwardIcon />} title="App website permissions" />
+<hr />
+<h2 className="text-gray-500">For families</h2>
+<OptionTab icon={<TbHomeShield />} arrowIcon={<ArrowForwardIcon />} title="App website permissions" />
+
+<hr />
+<h2 className="text-gray-500">For professionals</h2>
+<OptionTab icon={<VscGraph />} arrowIcon={<ArrowForwardIcon />} title="Account type and tools" />
+<OptionTab icon={<MdOutlineVerified />} arrowIcon={<ArrowForwardIcon />} title="Babagram Verified" />
+
+
+<hr />
+<h2 className="text-gray-500">Your orders and fundraisers</h2>
+<OptionTab icon={<TbHomeShield />} arrowIcon={<ArrowForwardIcon />} title="Fundraisers" />
+<OptionTab icon={<AiOutlineInbox />} arrowIcon={<ArrowForwardIcon />} title="Orders and payments" />
+
+
+<hr />
+<h2 className="text-gray-500">More info and support</h2>
+<OptionTab icon={<MdSupport />} arrowIcon={<ArrowForwardIcon />} title="Help" />
+<OptionTab icon={<MdOutlinePrivacyTip />} arrowIcon={<ArrowForwardIcon />} title="Privacy Center" />
+<OptionTab icon={<IoPersonOutline />} arrowIcon={<ArrowForwardIcon />} title="Account Status" />
+<OptionTab icon={<MdInfoOutline />} arrowIcon={<ArrowForwardIcon />} title="About" />
+
+
+<hr />
+<h2 className="text-gray-500">Also from Babagram</h2>
+<OptionTab icon={<FaWhatsapp />} arrowIcon={<ArrowForwardIcon />} title="WhatsApp" />
+<OptionTab icon={<FaThreads />} arrowIcon={<ArrowForwardIcon />} title="Threads" />
+<OptionTab icon={<RiFacebookCircleLine />} arrowIcon={<ArrowForwardIcon />} title="Facebook" />
+
+
+<hr />
+<h2 className="text-gray-500">Login</h2>
+<OptionTab  arrowIcon={<ArrowForwardIcon />} title="Add account" />
+<OptionTab arrowIcon={<ArrowForwardIcon />} title="Log out"/>
+
 
     </div>
   );
--- client/src/Components/AccountOptions/OptionTab.jsx
+++ client/src/Components/AccountOptions/OptionTab.jsx
@@ -1,11 +1,13 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
-const OptionTab = ({ icon, title, arrowIcon }) => {
+const OptionTab = ({ icon, title, arrowIcon}) => {
+  
   return (
     <div className="AccountCenterContainer flex-col justify-start items-start mt-[0.5em] gap-[0.5em] hover:bg-slate-700 rounded-[0.2em] p-[0.5em]">
       <div className="flex flex-row w-full items-center gap-[0.5em]">
         <div className="flex flex-row items-center gap-[0.5em]">
-          <div>{icon}</div>
+          <div>{icon || " "}</div>
           <h3>{title}</h3>
         </div>
         <div className="ml-auto"><Link><button>{arrowIcon}</button></Link></div>
