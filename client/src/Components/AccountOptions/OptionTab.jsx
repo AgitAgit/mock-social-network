c0f7fa922280f40784958fe1ab44b3e381cb8c8c
yosefhayim <yosefisabag+03@gmail.com>
2024-12-02 16:01:13 +0200

    fd

---
+++ client/src/Components/AccountOptions/OptionTab.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+const OptionTab = ({ icon, title, arrowIcon }) => {
+  return (
+    <div className="AccountCenterContainer flex-col justify-start items-start mt-[0.5em] gap-[0.5em] hover:bg-slate-700 rounded-[0.2em] p-[0.5em]">
+      <div className="flex flex-row w-full items-center gap-[0.5em]">
+        <div className="flex flex-row items-center gap-[0.5em]">
+          <div>{icon}</div>
+          <h3>{title}</h3>
+        </div>
+        <div className="ml-auto"><Link><button>{arrowIcon}</button></Link></div>
+      </div>
+    </div>
+  );
+};
+
+export default OptionTab;
