e05e5e1ef86e85bcab4167b93a02a53875a1562f
yosefhayim <yosefisabag+03@gmail.com>
2024-12-03 14:41:34 +0200

    fd

--- client/src/Components/Post/Post.jsx
+++ client/src/Components/Post/Post.jsx
@@ -3,6 +3,7 @@ import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import ShareIcon from "@mui/icons-material/Share";
 import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 import TurnedInNotIcon from "@mui/icons-material/TurnedInNot";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 const hoverColorIcon = {
   "&:hover": {
@@ -14,15 +15,32 @@ const hoverColorIcon = {
   color: "white",
 };
 
+const styleIcon = {
+  background: "#2A3751",
+  color: "white",
+  borderRadius: "1em",
+  width: "10vw",
+  height: "100%",
+  padding: "0.4em",
+  "&:hover": {
+    backgroundColor: "#050f20",
+  },
+};
+
 const Post = ({ post }) => {
   return (
     <div>
       <div key={post._id}>
         <div className="mb-[2em] flex flex-col items-center gap-[0.5em] rounded-[1em] p-[1.5em]">
-          <div className="creatorContainer flex w-full flex-row items-center justify-start gap-[0.5em]">
-            <img src={post.authorId.profilePic} alt="" className="w-[5vw]" />
-            <p>{post.authorId.username}</p>
-            <p className="text-gray-500">{timeSince(post.createdAt)}</p>
+          <div className="flex w-full flex-row items-center justify-between">
+            <div className="creatorContainer flex w-full flex-row items-center justify-start gap-[0.5em]">
+              <img src={post.authorId.profilePic} alt="" className="w-[5vw]" />
+              <p>{post.authorId.username}</p>
+              <p className="text-gray-500">{timeSince(post.createdAt)}</p>
+            </div>
+            <div>
+              <MoreVertIcon sx={styleIcon} />
+            </div>
           </div>
           <div className="postImageContainer flex flex-col">
             <img src={post.postImageUrl} alt="" className="rounded-[0em]" />
@@ -41,12 +59,12 @@ const Post = ({ post }) => {
               <TurnedInNotIcon className="cursor-pointer" sx={hoverColorIcon} />
             </div>
           </div>
-          <div className="flex w-full flex-row justify-start text-start">
+          <div className="flex w-full flex-row justify-start gap-[1em] text-start">
             <div>
-              <h2>{post.authorId.username}</h2>
+              <h2 className="w-full text-[0.7em]">{post.authorId.username}:</h2>
             </div>
             <div>
-              <p className="w-full text-[0.7em]">{post.content}</p>
+              <p className="w-full text-[0.7em]"> {post.content}</p>
             </div>
           </div>
           <div className="CommentsContainer bg-slate-500 p-[0.5em]">
@@ -55,7 +73,7 @@ const Post = ({ post }) => {
                 console.log(comment),
                 (
                   <div key={comment._id}>
-                    <div className="mb-[1em] flex flex-row items-center justify-start gap-[0.5em]">
+                    <div className="flex flex-row items-center justify-start gap-[0.5em]">
                       <div>
                         <img
                           src={comment.authorId.profilePic}
