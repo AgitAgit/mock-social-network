051ea0afb709701ef47e60de80858aa141b68e1b
yosefhayim <yosefisabag+03@gmail.com>
2024-12-03 19:11:46 +0200

    fd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
-
-//Import Components
-import Register from "./components/sign-in-and-register/Register.jsx";
-import SignIn from "./components/sign-in-and-register/SignIn.jsx";
 import Profile from "./components/Profile/Profile.jsx";
 import Homepage from "./components/Homepage/Homepage.jsx";
 import AllPosts from "./Components/AllPosts/AllPosts.jsx";
 import AccountOptions from "./Components/AccountOptions/AccountOptions.jsx";
-import Chatbox from "./Components/Chatbox/Chatbox.jsx";
 import AccountCenter from "./Components/AccountCenter/AccountCenter.jsx";
 import Saved from "./Components/Saved/Saved.jsx";
+import Login from "./Pages/Login/Login.jsx";
+import Register from "./Pages/Register/Register.jsx";
 
 // redux import
 import { store } from "./store/index.js";
@@ -22,13 +19,11 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/register" element={<Register />} />
           <Route path="/all-posts" element={<AllPosts />} />
           <Route path="/account-options" element={<AccountOptions />} />
-          <Route path="/chatbox" element={<Chatbox />} />
-
           <Route path="/account-center" element={<AccountCenter />} />
           <Route path="/saved" element={<Saved />} />
         </Routes>
