eea781979056be3b9333b1fdb3d67edac551ba73
ArielBitan <joekjoek33@gmail.com>
2024-12-03 12:20:05 +0200

    cleanup

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -30,7 +30,7 @@ async function addUser(req, res, next) {
       password: hashedPass,
       email,
       role,
-      imageUrl
+      imageUrl,
     });
     const newUser = await user.save();
     res.status(201).json({ mongoMessage: newUser });
@@ -39,32 +39,47 @@ async function addUser(req, res, next) {
   }
 }
 
-async function login(req, res, next){
-  try{
-      const { username, password } = req.body; //extract uname and pword from the req
-      if(!username || !password) return res.status(400).json({ message: "username and password are required..."});
-      const storedUser = await User.findOne({ username: username });//check if the user exists and extract it from the db
-      // console.log("stored user:", storedUser);
-      if(!storedUser) return res.status(400).json({message:`could not find user ${username}`});
-      const isValid = bcrypt.compareSync(password, storedUser.password);//use bcrypt to test if the login password matches the stored one
-      if(!isValid) return res.status(400).json({message:"Invalid password..."});
-      const token = jwt.sign(//generate a jwt token with payload containing the username, userId, and user role.
-          { user: { username, userId: storedUser._id, role:storedUser.role || "user" } },//this is the payload
-          secretKey,
-          { expiresIn: '1h' }
-      );
-      console.log("a user has logged in...")
-      res
-        .cookie("jwt", token, {//attach the jwt token to the response's cookie.
-          httpOnly: false,
-          secure: true, // Ensure the cookie is sent over HTTPS.(Do I need it to be true? I think we're sending http requests...)
-          sameSite: "strict", // Prevent cross-site requests.(Probably depends on the CORS middleware to define the allowed origin)
-          maxAge: 3600000, // Cookie lifespan of 1 hour (in milliseconds).
-        })
-        .status(200)
-        .json({message:`User ${username} logged in successfully.`, token});
-  } catch(error){
-      next(error);
+async function login(req, res, next) {
+  try {
+    const { username, password } = req.body; //extract uname and pword from the req
+    if (!username || !password)
+      return res
+        .status(400)
+        .json({ message: "username and password are required..." });
+    const storedUser = await User.findOne({ username: username }); //check if the user exists and extract it from the db
+    // console.log("stored user:", storedUser);
+    if (!storedUser)
+      return res
+        .status(400)
+        .json({ message: `could not find user ${username}` });
+    const isValid = bcrypt.compareSync(password, storedUser.password); //use bcrypt to test if the login password matches the stored one
+    if (!isValid)
+      return res.status(400).json({ message: "Invalid password..." });
+    const token = jwt.sign(
+      //generate a jwt token with payload containing the username, userId, and user role.
+      {
+        user: {
+          username,
+          userId: storedUser._id,
+          role: storedUser.role || "user",
+        },
+      }, //this is the payload
+      secretKey,
+      { expiresIn: "1h" }
+    );
+    console.log("a user has logged in...");
+    res
+      .cookie("jwt", token, {
+        //attach the jwt token to the response's cookie.
+        httpOnly: false,
+        secure: true, // Ensure the cookie is sent over HTTPS.(Do I need it to be true? I think we're sending http requests...)
+        sameSite: "strict", // Prevent cross-site requests.(Probably depends on the CORS middleware to define the allowed origin)
+        maxAge: 3600000, // Cookie lifespan of 1 hour (in milliseconds).
+      })
+      .status(200)
+      .json({ message: `User ${username} logged in successfully.`, token });
+  } catch (error) {
+    next(error);
   }
 }
 
