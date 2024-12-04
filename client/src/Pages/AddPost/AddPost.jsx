0e241ef51f8ab7cb6f3fcdf345bb93dd35b797bd
yosefhayim <yosefisabag+03@gmail.com>
2024-12-04 18:47:04 +0200

    fd

--- client/src/Pages/AddPost/AddPost.jsx
+++ client/src/Pages/AddPost/AddPost.jsx
@@ -1,5 +1,38 @@
+import PlaceholderImage from "/images/post-placeholder.svg";
+
 const AddPost = () => {
-  return <div>AddPost</div>;
+  return (
+    <div className="flex flex-col w-full">
+      <div className="flex w-full flex-row flex-wrap text-white">
+        <h2>New post</h2>
+        <div className="flex items-center">
+          <img src={PlaceholderImage} alt="" className="w-[30vw]" />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Write a caption or add a pull..."
+            className="w-full"
+          />
+        </div>
+        <div>
+          <p>icon placeholder</p>
+          <p>Tag people</p>
+        </div>
+        <div>
+          <p>Add music</p>
+        </div>
+        <div>
+          <p>Add location</p>
+        </div>
+        <div>
+          <button className="w-full rounded-[0.3em] bg-blue-600 pl-[0.4em] pr-[0.4em]">
+            Share
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddPost;
