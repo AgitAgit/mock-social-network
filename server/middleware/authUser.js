3426a8b813e92b5774121ec05f028f6ebc5a6be6
Amit Kubani <amitkubani@gmail.com>
2024-12-05 14:54:58 +0200

    refactored authUser

--- server/middleware/authUser.js
+++ server/middleware/authUser.js
@@ -24,6 +24,7 @@ function parseCookie(cookie) {
 async function authUser(req, res, next) {
     try {
         let token;
+        let decoded;
         if(req.headers.jwtAuthorization){
             token = req.headers.jwtAuthorization;
         }
@@ -31,23 +32,16 @@ async function authUser(req, res, next) {
             const cookieObject = parseCookie(req.headers.cookie);
             token = cookieObject.jwt;
         }
-        const decoded = await jwt.verify(token, "secretKey");
-    if (decoded) {
-      req.user = decoded.user;
-      console.log("authUser says: request by user:", decoded.user);
-      next();
-    } else {
-      //this is never reached. jsonwebtoken.verify throws an error if it fails. the catchall
-      //error handler could have a check for jsonwebtoken error type and proper handling.
-      //or I could add it before the next(error) in the catch section so if the error
-      //is of a known type it will be handled right here.
-      console.log("authUser stopped the request...");
-      return res.status(400).json({ message: "invalid token..." });
-    }
-  } catch (error) {
-    // console.log(error);
-    res.status(400).json({message: "invalid token..."});
-    next(error);
+        try{
+          decoded = jwt.verify(token, "secretKey");
+        } catch (error) {
+          return res.status(400).json({message: "invalid token..."});
+        }
+        req.user = decoded.user;
+        console.log("authUser says: request by user:", decoded.user);
+        next();
+    } catch (error) {
+      next(error);
   }
 }
 
